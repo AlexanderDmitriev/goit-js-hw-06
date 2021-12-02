be042531524cbffe19e796e6fc2363db66efa23f
AlexanderDmitriev <alex911007@inbox.ru>
2021-12-02 02:37:07 +0200

    Update task-02.js

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,22 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+function createList(spacecrafts){
+
+  let listView = document.createElement('div');
+  listView.classList.add('item');
+
+for(let i=0;i<spacecrafts.length;i++)
+{
+    let listViewItem=document.createElement('li');
+    listViewItem.appendChild(document.createTextNode(spacecrafts[i]));
+    listView.appendChild(listViewItem);
+}
+
+return listView;
+}
+
+const a = document.querySelector('#ingredients');
+const x=createList(ingredients);
+a.append(x);
+
