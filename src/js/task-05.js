d31ac09a2c7f8ce08639d11aaf7bce14214d987e
AlexanderDmitriev <alex911007@inbox.ru>
2021-12-03 19:51:44 +0200

    1

--- src/js/task-05.js
+++ src/js/task-05.js
@@ -5,4 +5,4 @@
 const textInput=document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
-textInput.addEventListener('input', () => { textOutput.textContent = ((textInput.value)||("Anonymous")); });
+textInput.addEventListener('input', () => { textOutput.textContent = ((textInput.value.trim())||("Anonymous")); });
